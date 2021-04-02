dac553ddeff3b140a8a2de705c882c2b0b14fd47
Oshan12345 <bsagor99@gmail.com>
2021-04-02 17:53:55 +0600

    first commit

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDv9X5t4QVx5ATHIKEArrt2xd4sOuY4obw",
+  authDomain: "book-voice.firebaseapp.com",
+  projectId: "book-voice",
+  storageBucket: "book-voice.appspot.com",
+  messagingSenderId: "604507523129",
+  appId: "1:604507523129:web:84b28d80e90e2f350da5e8",
+};
+
+export default firebaseConfig;
